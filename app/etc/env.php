3383efc319da9a6646771aed0d56f4562361239d
Alexey Bezpoyasny <alexey_b2007@ukr.net>
2019-11-08 09:32:33 +0200

    completed homework-8

---
+++ app/etc/env.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '81ba5e5e32eda3bdb834074fcc6225b1'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'alexey_bezpoyasny_local',
+                'username' => 'alexey_bezpoyasny_local',
+                'password' => 'alexey_bezpoyasny_local',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '40d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '40d_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'google_product' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'alexey-bezpoyasny.local'
+    ],
+    'install' => [
+        'date' => 'Wed, 06 Nov 2019 07:39:51 +0000'
+    ]
+];
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Alexeyb_mytheme_en_US: {
+        area: 'frontend',
+        name: 'Alexeyb/mytheme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+};
